a6c381164575077d3bcc1a5a924a9c0fd11e115f
Manvir Kaur <100617721+manvirk21@users.noreply.github.com>
2024-03-17 19:44:02 -0500

    Quiz Prototype
    There are two quizzes in this folder. One is a more basic one and the other has more stuff. There are pros and cons to both but we can discuss further.

---
+++ QuizGame/Quiz Basic/Scripts/game.gd
@@ -0,0 +1,64 @@
+extends Control
+
+@onready var DisplayText = $VBoxContainer/DisplayText
+@onready var ListItem = $VBoxContainer/ItemList
+@onready var RestartButton = $VBoxContainer/Button
+
+var items : Array = read_json_file("res://Assets/questions.json")
+var item : Dictionary
+var index_item : int = 0
+
+var correct : float = 0
+var wrong: int = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	refresh_scene()
+
+func read_json_file(filename):
+	var json_as_text = FileAccess.get_file_as_string(filename)
+	var json_data = JSON.parse_string(json_as_text)
+	print(json_data)
+	return json_data
+	
+func refresh_scene():
+	if index_item >= items.size():
+		show_result()
+	else:
+		show_question()
+		#displayScore()
+
+func show_question():
+	ListItem.show()
+	RestartButton.hide()
+	ListItem.clear()
+	item = items[index_item]
+	DisplayText.text = item.question
+	var options = item.options
+	for option in options:
+		ListItem.add_item(option)
+
+func show_result():
+	ListItem.hide()
+	RestartButton.show()
+	var score = round(correct/items.size()*100)
+	var greet
+	if score >= 80:
+		greet = "Congratulations! You passed and can have some candy"
+	else:
+		greet = "Oh no! You didn't get enough points..."
+	DisplayText.text = "{greet}! Your score is {score}%".format({"greet": greet, "score": score})
+
+
+func _on_item_list_item_selected(index):
+	if index == item.correctOptionIndex:
+		correct +=1
+	index_item +=1
+	refresh_scene()
+
+
+func _on_button_pressed():
+	correct = 0
+	index_item = 0	
+	refresh_scene()
