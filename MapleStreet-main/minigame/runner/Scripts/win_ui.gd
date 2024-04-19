00dddd11b7e991a605256858ae361beb5d6bc2f7
Manvir Kaur <manvirkaur@ku.edu>
2024-04-19 11:55:21 -0500

    Runner update

--- MapleStreet-main/minigame/runner/Scripts/win_ui.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_restart_pressed():
-	get_tree().change_scene_to_file("res://minigame/runner/Scenes/runner.tscn")
-
-
-func _on_continue_pressed():
-	get_tree().paused = false
