c379a0ecfe354e52b66fea15f34cc2958ccf63c9
Ava Greene <565avagreene565@gmail.com>
2024-04-20 12:13:45 -0500

    randomized cheese change

--- MapleStreet-main/minigame/maze/Scripts/Cheese.gd
+++ MapleStreet-main/minigame/maze/Scripts/Cheese.gd
@@ -5,6 +5,7 @@ extends Node2D
 func _on_pickup_body_entered(_body):
 	Cheese.visible = false
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- MapleStreet-main/minigame/maze/Scripts/maze.gd
+++ MapleStreet-main/minigame/maze/Scripts/maze.gd
@@ -3,14 +3,32 @@ extends Node2D
 @export var MapleStreet : PackedScene
 @onready var Cheese: Node2D = find_child("Cheese")
 var rng = RandomNumberGenerator.new()
+var position1 = Vector2(1391, 578)
+var position2 = Vector2(1315, 415)
+var position3 = Vector2(733, 34)
+var position4 = Vector2(1185, 643)
+var position5 = Vector2(711, 701)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_Teddy_settings()
-	var rndX = rng.randi_range(741, 1396) #creating randoom x position for cheese, the numbers are the bounds of the maze
-	var rndY = rng.randi_range(34, 724) #creating randoom y position for cheese, the numbers are the bounds of the maze
-	Cheese.position = Vector2(rndX, rndY)
+	Cheese.position = position1
+	var randNum = rng.randi_range(0, 4)
+	
+	if randNum == 0:
+		Cheese.position = position1
+	elif randNum == 1:
+		Cheese.position = position2
+	elif randNum == 2:
+		Cheese.position = position3
+	elif randNum == 3:
+		Cheese.position = position4
+	else:
+		Cheese.position = position5
+	#var rndX = rng.randi_range(741, 1396) #creating randoom x position for cheese, the numbers are the bounds of the maze
+	#var rndY = rng.randi_range(34, 724) #creating randoom y position for cheese, the numbers are the bounds of the maze
+	#Cheese.position = Vector2(rndX, rndY)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
