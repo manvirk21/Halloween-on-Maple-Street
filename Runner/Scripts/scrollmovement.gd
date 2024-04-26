e9bd9a2ca7fbfb3926a7668a6347a9f244134f0a
Ava Greene <565avagreene565@gmail.com>
2024-04-26 12:11:25 -0500

    Delete Runner directory

--- Runner/Scripts/scrollmovement.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-@export var scroll_speed = 8
-
-func move():
-	position.x -= scroll_speed
--- Runner/Scripts/updatescoreui.gd
+++
@@ -1,9 +0,0 @@
-extends RichTextLabel
-
-
-func _ready():
-	Signals.connect("updatescore", updatescore)
-	
-func updatescore(score):
-	self.text = str(score)
-
