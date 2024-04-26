51f0a84940a455e5e619689d8ecaf942e0312a60
Ava Greene <565avagreene565@gmail.com>
2024-04-25 22:08:44 -0500

    Add files via upload

--- MapleStreet-main/minigame/maze/Scripts/mazefinish.gd
+++ MapleStreet-main/minigame/maze/Scripts/mazefinish.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var Congrats: Sprite2D = find_child("FinishScreen") #set variable Congrats for the FinishScreen Sprite2D
+@onready var Congrats: Sprite2D = get_node("MiniGameFinish/FinishScreen") #set variable Congrats for the FinishScreen Sprite2D
 @onready var Cheese: Sprite2D = get_node("../Cheese/CheeseSprite") 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,11 +17,3 @@ func _on_finish_area_2d_body_entered(body):
 		Congrats.visible = true 
 	else:
 		pass
-
-func _on_continue_game_pressed():
-	get_tree().change_scene_to_file("res://maple_street/Scenes/Maple_Street.tscn")
-
-
-
-func _on_try_again_pressed():
-	get_tree().change_scene_to_file("res://minigame/maze/Scenes/maze.tscn")
