53bdea4a16e5c6654d0fb4e4aada53b24444962a
Manvir Kaur <manvirkaur@ku.edu>
2024-04-28 10:35:05 -0500

    Added title and temp scenes for settings and credits

--- MapleStreet-main/maple_street/Scripts/TitleScreen.gd
+++ MapleStreet-main/maple_street/Scripts/TitleScreen.gd
@@ -4,3 +4,10 @@ extends Control
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://maple_street/Scenes/Maple_Street.tscn")
 
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://maple_street/Scenes/settings.tscn")
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://maple_street/Scenes/credits.tscn")
