9ac619b95f6ef5005695634ab03e905eaf84618a
Magaly Camacho <130792592+magaly3118@users.noreply.github.com>
2024-04-30 19:18:14 -0500

    Added mockup for inventory popup, and made it so music keeps playing when game is paused

--- MapleStreet-main/maple_street/Scripts/UI/UI.gd
+++ MapleStreet-main/maple_street/Scripts/UI/UI.gd
@@ -1,5 +1,14 @@
 extends CanvasLayer
 
+var MINIGAMES = Global.VALID_MINIGAMES
+@onready var CandySprites : Dictionary = {
+	"maze" : $Inventory/MazeCandy,
+	"bowling" : $Inventory/BowlingCandy,
+	"runner" : $Inventory/RunnerCandy,
+	"cupswap" : $Inventory/CupSwapCandy,
+	"quiz" : $Inventory/QuizCandy
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -13,21 +22,39 @@ func toggle_inventory():
 	# show inventory
 	if not Global.is_game_paused:
 		show_inventory()
-		$Container/temp_inventory.visible = true
 		
 	# hide inventory
 	elif Global.current_pauser == Global.pausers.INVENTORY_BUTTON:
 		hide_inventory()
-		$Container/temp_inventory.visible = false
 
 # pause street and show inventory
 func show_inventory():
-	pause(Global.pausers.INVENTORY_BUTTON)
+	if not Global.is_game_paused:
+		# pause game 
+		pause(Global.pausers.INVENTORY_BUTTON)
+		
+		# show inventory background
+		$Inventory/Background.visible = true
+		
+		# show candy that has been collected only
+		var candy_status = Save.get_candy_inventory()
+		for minigame in MINIGAMES:
+			if candy_status[minigame] > 0:
+				CandySprites[minigame].visible = true
 
 # resume street and hide inventory
 func hide_inventory():
-	resume()
+	if Global.current_pauser == Global.pausers.INVENTORY_BUTTON:
+		# hide inventory background
+		$Inventory/Background.visible = false
 	
+		# hide candy
+		for minigame in MINIGAMES:
+			CandySprites[minigame].visible = false
+		
+		# resume game
+		resume()
+
 # pause street 
 func pause(pauser):
 	Global.is_game_paused = true
@@ -44,10 +71,10 @@ func resume():
 func _on_help_button_pressed():
 	# show inventory
 	if not Global.is_game_paused:
-		show_inventory()
-		$Container/temp_help.visible = true
+		pause(Global.pausers.HELP_BUTTON)
+		$Help/temp_help.visible = true
 		
 	# hide inventory
-	elif Global.current_pauser == Global.pausers.INVENTORY_BUTTON:
-		hide_inventory()
-		$Container/temp_help.visible = false
+	elif Global.current_pauser == Global.pausers.HELP_BUTTON:
+		resume()
+		$Help/temp_help.visible = false
