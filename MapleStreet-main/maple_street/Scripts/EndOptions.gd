5a0f0803a053ade471386d9374d85349893ba4d2
Ava Greene <565avagreene565@gmail.com>
2024-05-03 02:19:09 -0500

    Edited endoptions.gd

--- MapleStreet-main/maple_street/Scripts/EndOptions.gd
+++ MapleStreet-main/maple_street/Scripts/EndOptions.gd
@@ -2,11 +2,28 @@ extends Control
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://maple_street/Scenes/Maple_Street.tscn")
+	print(Save.is_intro_viewed())
+	if Save.is_intro_viewed():
+		get_tree().change_scene_to_file("res://maple_street/Scenes/Maple_Street.tscn")
+		return
+	else:
+		get_tree().change_scene_to_file("res://intro_outro/Scenes/intro_outro.tscn")
+		return
+
 
 func _on_settings_pressed():
-	get_tree().quit()
+	$ResetWindow.visible = true
 
 
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://credits/scenes/credits.tscn")
+
+
+func _on_no_pressed():
+	$ResetWindow.visible = false
+
+
+func _on_yes_pressed():
+	Save.reset()
+	$ResetWindow.visible = false
+	get_tree().change_scene_to_file("res://maple_street/Scenes/TitleScreen.tscn")
